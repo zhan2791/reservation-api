904017604f7fed25a6412ef37c3a2ecc8cf3477e
Lucy Zhang <luciazhang890@gmail.com>
2026-02-02 13:52:39 +1030

    feat: add health endpoint and permit all security config

--- src/main/java/com/pm/reservation/controller/ReservationController.java
+++ src/main/java/com/pm/reservation/controller/ReservationController.java
@@ -1,6 +1,7 @@
 package com.pm.reservation.controller;
 
 import com.pm.reservation.Service.ReservationService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class ReservationController {
     private final ReservationService reservationService;
     public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
     }
 
 }
